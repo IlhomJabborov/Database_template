b032886bc507c2536be8edbb30566e093e3deabd
Ilhom Jabborov <jabborovilhom300@gmail.com>
2023-03-08 18:31:09 +0500

    brasil pochta indexsini o'zgartirdim

--- yuk_tashish.sql
+++ yuk_tashish.sql
@@ -8,5 +8,5 @@ create table korxona (
 );
 insert into korxona (id, shirkat_nomi, shahar, davlat, pochta_index, telefon_raqam) values (1, 'foo.Cu', 'Mixco', 'Guatemala', '25029', '+2 (901) 793-7833');
 insert into korxona (id, shirkat_nomi, shahar, davlat, pochta_index, telefon_raqam) values (2, 'foo.Ind', 'Samir', 'Indonesia', '20123', '+62 (560) 535-4800');
-insert into korxona (id, shirkat_nomi, shahar, davlat, pochta_index, telefon_raqam) values (3, 'foo.Brl', 'Jaguarari', 'Brazil', '28001', '+55 (885) 763-1523');
+insert into korxona (id, shirkat_nomi, shahar, davlat, pochta_index, telefon_raqam) values (3, 'foo.Brl', 'Jaguarari', 'Brazil', '21011', '+55 (885) 763-1523');
 insert into korxona (id, shirkat_nomi, shahar, davlat, pochta_index, telefon_raqam) values (4, 'foo.ru', 'Privolzhsk', 'Russia', '10131', '+7 (392) 952-5564');
