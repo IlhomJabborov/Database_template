e796ce06c0107fea9f23951dc39c5c9841532342
Ilhom Jabborov <127211749+JabborovRoboCoder@users.noreply.github.com>
2023-03-08 18:04:57 +0500

    yangilash korxona jadvali

--- yuk_tashish.sql
+++ yuk_tashish.sql
@@ -6,7 +6,7 @@ create table korxona (
 	pochta_index VARCHAR(50),
 	telefon_raqam VARCHAR(50)
 );
-insert into korxona (id, shirkat_nomi, shahar, davlat, pochta_index, telefon_raqam) values (1, 'foo.Cu', 'Mixco', 'Guatemala', '25029', '+998 (90) 793-7833');
+insert into korxona (id, shirkat_nomi, shahar, davlat, pochta_index, telefon_raqam) values (1, 'foo.Cu', 'Mixco', 'Guatemala', '25029', '+2 (901) 793-7833');
 insert into korxona (id, shirkat_nomi, shahar, davlat, pochta_index, telefon_raqam) values (2, 'foo.Ind', 'Samir', 'Indonesia', '20123', '+62 (560) 535-4800');
 insert into korxona (id, shirkat_nomi, shahar, davlat, pochta_index, telefon_raqam) values (3, 'foo.Brl', 'Jaguarari', 'Brazil', '28001', '+55 (885) 763-1523');
 insert into korxona (id, shirkat_nomi, shahar, davlat, pochta_index, telefon_raqam) values (4, 'foo.ru', 'Privolzhsk', 'Russia', '10131', '+7 (392) 952-5564');
